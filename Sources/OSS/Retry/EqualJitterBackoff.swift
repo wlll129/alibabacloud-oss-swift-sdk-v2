3151505d08c88ea134df1a063f8f2fc60378bf38
ws <wb-ws775196@alibaba-inc.com>
2025-05-07 10:30:44 +0800

    add public init method

--- Sources/OSS/Retry/EqualJitterBackoff.swift
+++ Sources/OSS/Retry/EqualJitterBackoff.swift
@@ -3,6 +3,11 @@ import Foundation
 public struct EqualJitterBackoff: Backoff {
     let baseDelay: TimeInterval
     let maxBackoff: TimeInterval
+    
+    public init(baseDelay: TimeInterval, maxBackoff: TimeInterval) {
+        self.baseDelay = baseDelay
+        self.maxBackoff = maxBackoff
+    }
 
     public func backoffDelay(attempt: Int, error _: Error) -> TimeInterval {
         let ceil = min(pow(Double(2), Double(attempt) * baseDelay), maxBackoff)
